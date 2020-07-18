1452606d8e631a80508e18f007caf485033ca9d2
danebalia <dane.balia@sourcing-sa.co.za>
2020-07-18 23:03:46 +0200

    Fixed broken paddle knee jerk rejection

--- src/scenes/mainScene.js
+++ src/scenes/mainScene.js
@@ -5,21 +5,14 @@ class MainScene extends Phaser.Scene {
    cursors = "";
    blueBricks = "";
    redBricks = "";
+   gameStarted = false;
+
 
   constructor() {
     super({ key: 'MainScene' })
   }
 
   create() {
-
-    // Brick Layer One
-    const bricks = 8;
-    const brickSize = 68;
-
-    const brickBlueHeight = 100;
-    let brickBlueX = 95;
-    const brickGreenHeight = 150;
-    let brickGreenX = 95;
 
     // Static World
     this.add.image(400, 300, 'background');
@@ -61,23 +54,40 @@ class MainScene extends Phaser.Scene {
       }
     });
 
+    this.purpleBricks = this.physics.add.group({
+      key: 'purple',
+      repeat: 9,
+      immovable: true,
+      setXY: {
+        x: 80,
+        y: 190,
+        stepX: 70
+      }
+    });
 
-    this.ball = this.physics.add.sprite(this.game.config.width / 2, this.game.config.height - 100, 'ball');
-    this.ball.setCollideWorldBounds(true).setBounce(1);
-    this.ball.setSize(0.5);
 
-    this.player = this.physics.add.sprite(400, 600, 'paddle').setImmovable();
-    this.player.setCollideWorldBounds(true);
-    this.player.body.setVelocityX(0);
+  // Ball physics
+  this.ball = this.physics.add.sprite(this.game.config.width / 2, this.game.config.height - 100, 'ball');
+  this.ball.setCollideWorldBounds(true).setBounce(1);
+  this.ball.setSize(0.5);
 
-    this.cursors = this.input.keyboard.createCursorKeys();
+  // Paddle physics
+  this.player = this.physics.add.sprite(400, 600, 'paddle').setImmovable(true);
+  this.player.setCollideWorldBounds(true);
+  this.player.body.setVelocityX(1);
+
+  // User input
+  this.cursors = this.input.keyboard.createCursorKeys();
 
   this.ball.setBounce(1, 1);
   this.physics.world.checkCollision.down = false;
 
+  // Collission physics
+  this.physics.add.collider(this.player, this.ball,  this.hitPaddle, null, this);
   this.physics.add.collider(this.ball, this.blueBricks, this.hitBrick, null, this);
   this.physics.add.collider(this.ball, this.yellowBricks, this.hitBrick, null, this);
   this.physics.add.collider(this.ball, this.redBricks, this.hitBrick, null, this);
+  this.physics.add.collider(this.ball, this.purpleBricks, this.hitBrick, null, this);
   
   } 
 
@@ -90,22 +100,39 @@ class MainScene extends Phaser.Scene {
   }
 
   update() {
-    console.log("Hello");
 
     if (this.cursors.left.isDown) {
       this.player.body.setVelocityX(-350);
     } else if (this.cursors.right.isDown) {
       this.player.body.setVelocityX(350);
-      this.ball.setVelocityY(-200);
+      
+      if (!this.gameStarted) {
+        this.gameStarted = true;
+        this.ball.setVelocityY(-200);
+      }
+      
     }
 
+  }
+
+  hitPaddle(ball, paddle)
+  {
+    if (ball.body.velocity.x === 0) {
+      let randNum = Math.random();
+      console.log(randNum);
+      if (randNum >= 0.5) {
+        ball.body.setVelocityX(150);
+      } else {
+        ball.body.setVelocityX(-150);
+      }
+    }
   }
 
   hitBrick(ball, brick) {
     brick.disableBody(true, true);
   
     if (ball.body.velocity.x === 0) {
-      randNum = Math.random();
+      let randNum = Math.random();
       if (randNum >= 0.5) {
         ball.body.setVelocityX(150);
       } else {
